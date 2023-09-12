73d5bcbd6de0b513bc3493b17b58fac20284f314
Lavanya <lavanya.goel12@gmail.com>
2023-09-12 11:19:46 -0400

    revert

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,6 @@ const calc = new Calculator();
 
 const result_add = calc.add(2, 3);
 const result_sub = calc.subtract(2, 3);
-const result_div = calc.divide(6, 2);
 console.log(`result_add = ${result_add}`);
 console.log(`result_sub = ${result_sub}`);
-console.log(`result_div = ${result_div}`);
 
