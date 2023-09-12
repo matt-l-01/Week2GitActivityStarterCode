b05533f7cbd02e873d86b5834b977defd4f512f8
matt-l-01 <46176691+matt-l-01@users.noreply.github.com>
2023-09-12 11:13:55 -0400

    Added multiply operation

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ const calc = new Calculator();
 
 const result_add = calc.add(2, 3);
 const result_sub = calc.subtract(2, 3);
+const result_mult = calc.multiply(2, 3);
 console.log(`result_add = ${result_add}`);
 console.log(`result_sub = ${result_sub}`);
+console.log(`result_mult = ${result_mult}`);
 
